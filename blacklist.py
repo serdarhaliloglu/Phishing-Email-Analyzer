efc426a020db0bf94109b8b389714b3fcc4b0c1a
Serdar Haliloğlu <serdarrhaliloglu@gmail.com>
2017-05-07 16:03:36 +0300

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python2.7
-# -*- coding: utf-8 -*-
-
 import dns.resolver
 import sys
 import os
@@ -12,16 +9,10 @@ import re
 import socket
 from urllib2 import urlopen
 
-""" 
-Important! 
-
---->>>> Requires dnspython package. You can easily install it by typing this command. 
-
-sudo pip install dnspython
-
-"""
-
-bls = [ "short.rbl.jp", "owps.dnsbl.net.aupbl.spamhaus.org","bogons.cymru.com",
+bls = [ "dnsrbl.org","rbl.megarbl.net","rbl.realtimeblacklist.com","bl.blocklist.de","bad.psky.me",
+		"escalations.dnsbl.sorbs.net","stabl.rbl.webiron.net",
+		"cabl.rbl.webiron.net","babl.rbl.webiron.net","short.rbl.jp", 
+		"owps.dnsbl.net.aupbl.spamhaus.org","bogons.cymru.com",
 		"rdts.dnsbl.net.au","web.dnsbl.sorbs.net","dnsbl.inps.de",
 		"korea.services.net","ix.dnsbl.manitu.net","dnsbl.njabl.org",
 		"spamlist.or.kr","spamrbl.imp.ch","proxy.block.transip.nl",
@@ -46,90 +37,31 @@ bls = [ "short.rbl.jp", "owps.dnsbl.net.aupbl.spamhaus.org","bogons.cymru.com",
 		"bl.deadbeef.com","combined.abuse.ch","dnsbl.dronebl.org",
 		"cdl.anti-spam.org.cn","blackholes.five-ten-sg.com","aspews.ext.sorbs.net",
 		"dnsbl-3.uceprotect.net","phishing.rbl.msrbl.net","relays.nether.net",
-		"torserver.tor.dnsbl.sectoor.de"
+		"torserver.tor.dnsbl.sectoor.de", "bl.score.senderscore.org",
+		"dnsbl.ahbl.org","rp.invaluement.com"] #setting of DNS Blacklist Servers
 
-	]
+var1 = var2 = var3 = var4 = var5 = 0 
 
+print 'Please enter an IP address or a Domain address'
+input = raw_input()
 
-def color(text, color_code):
-		if sys.platform == "win32" and os.getenv("TERM") != "xterm":
-				return text
+pat = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")   #ip validation regex
+test1 = pat.match(input)
+if test1:	#if the given is a valid ip address									
+	myIP=input  # set input to myIP
 
-		return '\x1b[%dm%s\x1b[0m' % (color_code, text)
+else:  #if the given is not a valid ip address, can be a valid domain address or invalid input
+	validate = re.compile("^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3})$") #domain validation regex
+	test2 = validate.match(input)  
+	if test2:  #if the given is a valid domain address
+		print '' #do nothing
+	else: #if the given is not a valid ip or domain address
+		print "\nInvalid IP or Domain address. Please enter the valid one."
+		sys.exit(0) #exit
 
-
-def red(text):
-		return color(text, 31)
-
-
-def blink(text):
-		return color(text, 5)
-
-
-def green(text):
-		return color(text, 32)
-
-
-def blue(text):
-		return color(text, 34)
-
-var1 = 0 
-var2 = 0 
-var3 = 0
-var4 = 0
-var5 = 0
-
-print "Click i or d for scanning"
-x = raw_input()
-if x == "i":
-		print "Write an ip address"
-		myIP = raw_input()
-
-		for bl in bls:
-				try:
-						my_resolver = dns.resolver.Resolver()
-						query = '.'.join(reversed(str(myIP).split("."))) + "." + bl
-						my_resolver.timeout = 2
-						my_resolver.lifetime = 2
-						answers = my_resolver.query(query, "A")
-						answer_txt = my_resolver.query(query, "TXT")
-						var5 = var5+1
-						print 'IP: %s IS listed in %s (%s: %s)' %(myIP, bl, answers[0], answer_txt[0])
-
-				except dns.resolver.NXDOMAIN:
-					var1= var1+1
-					print 'IP: %s is NOT listed in %s' %(myIP, bl)			 
-				except dns.resolver.Timeout:
-					var2= var2+1
-					print (blink('WARNING: Timeout querying ' + bl))
-
-				except dns.resolver.NoNameservers:
-					var3= var3+1
-					print (blink('WARNING: No nameservers for ' + bl))
-
-				except dns.resolver.NoAnswer:
-					var4= var4+1
-					print (blink('WARNING: No answer for ' + bl))
-		var6= var1 + var2 + var3 + var4 + var5
-		print
-		print '*****RESULT OF %s BLACKLIST SERVERS FOR %s*****' %(var6,myIP)
-		print 
-		print 'The IP address is not listed in %s BLServer(s).'  %(var1)      
-		print 'The IP address is listed in %s BLServer(s).'  %(var5) 
-		print     
-		print 'Timeout querying in %s BLServer(s).'  %(var2) 
-		print 'No answer for %s BLServer(s).'  %(var4) 
-		print 'No nameservers for %s BLServer(s).'  %(var3) 
-		print 
-
-else:
-		import socket
-		print "Write a domain address"
-		myDomain = raw_input()
-
-		for bl in bls:
-			try:
-				myIP = socket.gethostbyname(myDomain)
+for bl in bls:    #if the given is a valid ip or domain address start scanning process
+		try:	
+				myIP = socket.gethostbyname(input) #convert domain address to ip address
 				my_resolver = dns.resolver.Resolver()
 				query = '.'.join(reversed(str(myIP).split("."))) + "." + bl
 				my_resolver.timeout = 2
@@ -137,29 +69,32 @@ else:
 				answers = my_resolver.query(query, "A")
 				answer_txt = my_resolver.query(query, "TXT")
 				var5 = var5+1
-				print 'IP: %s IS listed in %s (%s: %s)' %(myDomain, bl, answers[0], answer_txt[0])
+				print 'IP: %s IS listed in %s (%s: %s)' %(myIP, bl, answers[0], answer_txt[0]) 
 
-			except dns.resolver.NXDOMAIN:
-				var1= var1+1
-				print 'IP: %s is NOT listed in %s' %(myIP, bl)
-			except dns.resolver.Timeout:
-				var2= var2+1
-				print (blink('WARNING: Timeout querying ' + bl))
-			except dns.resolver.NoNameservers:
-				var3= var3+1
-				print (blink('WARNING: No nameservers for ' + bl))
+		except socket.gaierror:
+			sys.exit("Invalid Domain address. Please enter the valid one.")  #exit, if convertion is false
 
-			except dns.resolver.NoAnswer:
-				var4= var4+1
-				print (blink('WARNING: No answer for ' + bl))
-		var6= var1 + var2 + var3 + var4 + var5
-		print
-		print '*****RESULT OF %s BLACKLIST SERVERS FOR %s*****' %(var6,myIP)
-		print 
-		print 'The IP address is not listed in %s BLServer(s).'  %(var1)      
-		print 'The IP address is listed in %s BLServer(s).'  %(var5) 
-		print     
-		print 'Timeout querying in %s BLServer(s).'  %(var2) 
-		print 'No answer for %s BLServer(s).'  %(var4) 
-		print 'No nameservers for %s BLServer(s).'  %(var3) 
-		print
+		except dns.resolver.NXDOMAIN:
+			var1= var1+1
+			print 'IP: %s is NOT listed in %s' %(myIP, bl)
+			 
+		except dns.resolver.Timeout:
+			var2= var2+1
+			print ('WARNING: Timeout querying ' + bl) 
+
+		except dns.resolver.NoNameservers:
+			var3= var3+1
+			print ('WARNING: No nameservers for ' + bl)
+
+		except dns.resolver.NoAnswer:
+			var4= var4+1
+			print ('WARNING: No answer for ' + bl) 
+
+var6= var1 + var2 + var3 + var4 + var5 			   # calcute total number of DNS Blacklist Servers
+
+print '\n*****RESULT OF %s BLACKLIST SERVERS FOR %s*****\n' %(var6,myIP) 		# print total number of DNS Blacklist Servers
+print 'The IP address is not listed in %s BLServer(s).'  %(var1)     		    # print the ip/domain is not listed how many of DNS Blacklist Servers
+print 'The IP address is listed in %s BLServer(s).\n'  %(var5)     			    # print the ip/domain is listed how many of DNS Blacklist Servers
+print 'Timeout querying in %s BLServer(s).'  %(var2)                            # print the number of timeouts from DNS Blacklist Servers
+print 'No answer for %s BLServer(s).'  %(var4)                                  # print the number of no answer from DNS Blacklist Servers
+print 'No nameservers for %s BLServer(s).\n'  %(var3) 
